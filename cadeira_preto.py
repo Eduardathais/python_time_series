a70447f55baabf5bfaff9a71c45357ab1e155d4e
Eduardathais <eduardathais18@gmail.com>
2025-08-25 20:28:38 -0300

    cabecalhos e dockstring

---
+++ cadeira_preto.py
@@ -0,0 +1,16 @@
+# em python o que é um cabeçalho de um arquivo python e pra que serve? qual a diferença pra dockstring e pra que serve a dockstring
+
+# Um cabeçalho de um arquivo Python geralmente inclui informações como o nome do arquivo, a descrição do que o arquivo faz, o autor, 
+# a data de criação e outras informações relevantes. Ele serve para fornecer 
+# contexto sobre o arquivo e facilitar a compreensão do seu propósito para outros desenvolvedores que possam ler ou usar o código no futuro.
+# Em requisições web, são dados adicionais que acompanham o pedido ou a resposta, 
+# transmitindo informações sobre a conexão, o conteúdo e o cliente. 
+
+# Uma docstring, por outro lado, é uma string de documentação que aparece logo após a definição de um módulo, classe ou função. 
+# Ela é usada para descrever o que o módulo, classe ou função faz, quais são seus parâmetros,
+# o que ela retorna, entre outras informações úteis. As docstrings são acessíveis em tempo de execução através do atributo __doc__.
+# A principal diferença entre um cabeçalho de arquivo e uma docstring é que o cabeçalho é mais geral e fornece informações sobre o arquivo como um todo,
+# enquanto a docstring é específica para o módulo, classe ou função e fornece detalhes sobre seu funcionamento 
+
+# docstrings são armazenadas no atributo doc do objeto e são acessíveis a partir do código em tempo de execução. 
+# Isso significa que você pode acessar a documentação de uma função, por exemplo, diretamente do código.
